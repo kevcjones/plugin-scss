01cf315f7ded69c2540b2e8e4f9e58991ab12477
Kevin Jones <kevin.jones@nucleuscentral.com>
2016-02-21 12:20:12 +0000

    As per guybedford feedback - Made use to the this.builder - Boiled down duplication code - moved logic from fetch to translate (much cleaner now too)

--- src/index.js
+++ src/index.js
@@ -1,59 +1,20 @@
 /* global __moduleName */
 let fetch;
 let translate;
-let styles = {};
 
-if (typeof window !== 'undefined') {
-  fetch = function(load){
+fetch = function(load){ return ""; };
 
-    return System.import('./sass-inject', { name: __moduleName })
+translate = function(load,opts) {
+    load.metadata.format = 'amd';
+
+    return System.import(this.builder ? './sass-inject-build' : './sass-inject', { name: __moduleName })
       .then(function(inject){
         return inject.default(load);
       })
       .then(function(css){
-        return styles = css;
+        const output = 'def' + 'ine(function() {\nreturn "' + css.trim().replace('\n', '') + '";\n});';
+        return (!!css) ? output : ''
       });
-  };
-  translate = function(load) {
-      load.metadata.format = 'amd';
-      return new Promise (function (resolve, reject) {
-        const css = styles.trim().replace('\n', '');
-        const output = 'def' + 'ine(function() {\nreturn "' + css + '";\n});';
-        if (!css){
-          reject('');
-        }
-        else {
-          resolve(output);
-        }
-      });
-  };
-
-} else {
-
-  fetch = function bundler(load, opts) {
-    return System.import('./sass-inject-build', { name: __moduleName })
-      .then(function(builder) {return builder.default.call(System, load, opts);})
-      .then(function(css){
-          // i'm not thrilled here but i cant see a way yet to pass between hooks
-          // since on bundle it appears to lose handle ALL the fetch in a single sitting
-          return styles[load.name] = css.replace(/\n$/, '');
-      });
-
-  };
-
-  translate = function(load) {
-      load.metadata.format = 'amd';
-      return new Promise(function(resolve, reject) {
-        const css = styles[load.name].trim().replace('\n', '');
-        const output = 'def' + 'ine(function() {\nreturn "' + css + '";\n});';
-        if (!css) {
-          reject('');
-        }
-        else {
-          resolve(output);
-        }
-      });
-  };
-}
+};
 
 export { fetch, translate };
