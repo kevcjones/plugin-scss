1a1b096db4aad4aa346e0b01edc550645d7a16e9
Kevin Jones <kevin.jones@nucleuscentral.com>
2016-04-10 22:15:50 +0100

    test+fix: folder with space issue fixed (windows)
    Copied from https://github.com/mobilexag/plugin-sass/issues/44

--- test/bundleme.js
+++ test/bundleme.js
@@ -1,6 +1,7 @@
 import test from 'test/styles/test.scss!';
 import test_sass from 'test/styles/test_sass.sass!scss';
 import test_other from 'test/styles/test_other.scss!';
+import test_spaced from 'test/styles/spaced directory/spaced.scss!';
 
 var injectCSS = function(c) {
     var d = document,
@@ -17,5 +18,6 @@ var injectCSS = function(c) {
 injectCSS(test);
 injectCSS(test_sass);
 injectCSS(test_other);
+injectCSS(test_spaced);
 
 console.log('I am bundled');
