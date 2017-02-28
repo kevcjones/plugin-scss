254c68a91dac276d0f50dd00166d1f84a8283760
Timothy Ruhle <timothy.ruhle@intellipharm.com.au>
2017-02-28 15:03:22 +1000

    esm format is not working

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ translate = function(load) {
             return inject.default(load);
         })
         .then(function(css) {
-            if (System.transpiler === false) {
+//             if (System.transpiler === false) {
                 load.metadata.format = 'amd';
                 const output = 'def' + 'ine(function() {\nreturn "' + css.trim().replace('\n', '') + '";\n});';
                 return (!!css) ? output : '';
-            }
+//             }
 
-            load.metadata.format = 'esm';
-            return 'export default "' + css.trim().replace('\n', '') + '"';
+//             load.metadata.format = 'esm';
+//             return 'export default "' + css.trim().replace('\n', '') + '"';
         });
 };
 
